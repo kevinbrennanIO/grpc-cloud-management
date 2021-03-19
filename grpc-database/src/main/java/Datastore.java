0f1318000723726196776b01956902667bb4a4ff
Kevin <kevdj2000@gmail.com>
2021-03-19 00:10:20 +0000

    grpc-database rework

---
+++ grpc-database/src/main/java/Datastore.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Datastore {
+
+    List<String> centralindia = Arrays.asList("big-DB-India", "testing-India-123", "indian-Company-Prod", "gov-india-db");
+    List<String> japaneast = Arrays.asList("big-DB-Japan", "testing-Japan-123", "japan-Company-Prod", "gov-japan-db");
+    List<String> ukwest = Arrays.asList("big-DB-Uk", "testing-uk-123", "uk-Company-Prod", "gov-uk-db");
+    List<String> regions = Arrays.asList("centralindia", "japaneast", "ukwest");
+
+    String userName = "kevin";
+    String password = "password";
+
+    String dummyToken = "cvd3645wtpg98vhw4o39";
+}
